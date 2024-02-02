1afa33459996c55c1e19c88fb677d126db4a8054
cuongh1799 <cuongghuynhh1009@gmail.com>
2024-02-02 22:26:57 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace newVIM
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cursorXLabel = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cursorYLabel = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.cursorXLabel);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.cursorYLabel);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(604, 368);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(104, 67);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(146, 38);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Position X:";
+            // 
+            // cursorXLabel
+            // 
+            this.cursorXLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cursorXLabel.Location = new System.Drawing.Point(375, 63);
+            this.cursorXLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.cursorXLabel.Name = "cursorXLabel";
+            this.cursorXLabel.Size = new System.Drawing.Size(83, 41);
+            this.cursorXLabel.TabIndex = 2;
+            this.cursorXLabel.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(104, 180);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(146, 41);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Position Y:";
+            // 
+            // cursorYLabel
+            // 
+            this.cursorYLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cursorYLabel.Location = new System.Drawing.Point(375, 180);
+            this.cursorYLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.cursorYLabel.Name = "cursorYLabel";
+            this.cursorYLabel.Size = new System.Drawing.Size(83, 41);
+            this.cursorYLabel.TabIndex = 4;
+            this.cursorYLabel.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label cursorXLabel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label cursorYLabel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Windows.Input;
+using System.Diagnostics;
+
+namespace newVIM
+{
+    public partial class Form1 : Form
+    {
+        private const int WH_KEYBOARD_LL = 13;
+        private const int WM_KEYDOWN = 0x0100;
+        private const int WM_KEYUP = 0X0101;
+        private static LowLevelKeyboardProc _proc = HookCallback;
+        private static IntPtr _hookID = IntPtr.Zero;
+        private const uint MOUSEEVENTF_LEFTDOWN = 0x0002;
+        private const uint MOUSEEVENTF_LEFTUP = 0x0004;
+        private const uint MOUSEEVENTF_RIGHTDOWN = 0x0008;
+        private const uint MOUSEEVENTF_RIGHTUP = 0x0010;
+
+
+        public Form1()
+        {
+            _hookID = SetHook(_proc);
+            InitializeComponent();
+        }
+
+        private static IntPtr SetHook(LowLevelKeyboardProc proc)
+        {
+            using (Process curProcess = Process.GetCurrentProcess())
+            using (ProcessModule curModule = curProcess.MainModule)
+            {
+                return SetWindowsHookEx(WH_KEYBOARD_LL, proc, GetModuleHandle(curModule.ModuleName), 0);
+            }
+        }
+
+        private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+        private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
+            {
+                int vkCode = Marshal.ReadInt32(lParam);
+                Keys key = (Keys)vkCode;
+                int moveAmount = 30; // Change this to the amount you want the cursor to move
+
+                switch (key)
+                {
+                    case Keys.W:
+                        Cursor.Position = new Point(Cursor.Position.X, Cursor.Position.Y - moveAmount);
+                        break;
+                    case Keys.A:
+                        Cursor.Position = new Point(Cursor.Position.X - moveAmount, Cursor.Position.Y);
+                        break;
+                    case Keys.S:
+                        Cursor.Position = new Point(Cursor.Position.X, Cursor.Position.Y + moveAmount);
+                        break;
+                    case Keys.D:
+                        Cursor.Position = new Point(Cursor.Position.X + moveAmount, Cursor.Position.Y);
+                        break;
+                    case Keys.Enter:
+                        // Simulate right click
+                        mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+                        mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+                        break;
+                }
+            }
+            else if (nCode >= 0 && wParam == (IntPtr)WM_KEYUP)
+            {
+                int vkCode = Marshal.ReadInt32(lParam);
+                Keys key = (Keys)vkCode;
+
+                switch (key)
+                {
+                    case Keys.Enter:
+                        // Simulate release of mouse button
+                        mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+                        break;
+                }
+            }
+            return CallNextHookEx(_hookID, nCode, wParam, lParam);
+        }
+
+
+        // Importing function from Windows API
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint cButtons, uint dwExtraInfo);
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // This constantly activates the cursorTracker()
+            // Thus keeps the cursorTracker X and Y updated
+            Thread trackerThread = new Thread(cursorTracker);
+            trackerThread.Start();
+        }
+
+        // Track the cursor's position X and Y
+        private void cursorTracker()
+        {
+            while (true)
+            {
+                int cursorX = MousePosition.X;
+                int cursorY = MousePosition.Y;
+                cursorXLabel.Text = cursorX.ToString();
+                cursorYLabel.Text = cursorY.ToString();
+            }
+        } 
+    }
+}
